5959f6ca2b7a9a7c481d5f643dced51bf654e44d
David Frischknecht <fishnet37222@outlook.com>
2026-06-29 20:43:54 -0400

    Initialize and shutdown GDI+ in application entry point
    Added GDI+ initialization and cleanup in `wWinMain`, displaying an error and exiting if initialization fails. Updated project settings to recognize "gdiplus" as a valid word.

--- MinesweeperWin32/App.cpp
+++ MinesweeperWin32/App.cpp
@@ -5,11 +5,22 @@
 #include "pch.h"
 #include "MainWindow.h"
 #include "resource.h"
+#include <gdiplus.h>
+#pragma comment(lib, "gdiplus.lib")
 
 #pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 int WINAPI wWinMain(const HINSTANCE hInstance, [[maybe_unused]] HINSTANCE hPrevInstance, [[maybe_unused]] PWSTR lpCmdLine, const int nShowCmd)
 {
+	// Initialize GDI+
+	const Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR gdiplusToken;
+	if (Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr) != Gdiplus::Ok)
+	{
+		MessageBoxW(nullptr, L"Failed to initialize GDI+.", L"Error", MB_OK | MB_ICONERROR);
+		return -1;
+	}
+
 	const auto hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR));
 
 	const auto mainWindow = MainWindow_Create(hInstance);
@@ -33,5 +44,6 @@ int WINAPI wWinMain(const HINSTANCE hInstance, [[maybe_unused]] HINSTANCE hPrevI
 		}
 	}
 
+	Gdiplus::GdiplusShutdown(gdiplusToken);
 	return 0;
 }
